88ee6b0e6198b50bd07ee5792b46d4d196346015
gj701763 <gj701763@gmail.com>
2024-11-13 12:30:08 +0530

    DSA

--- demo.js
+++ demo.js
@@ -1,23 +1,2 @@
-function demo(nums) {
-    if (nums.length <= 1) return nums;
 
-    let min = Math.min(...nums);
-
-    let max = Math.max(...nums);
-
-    let count = new Array(max - min + 1).fill(0);
-    for (let num of nums) {
-        count[num - min]++;
-    }
-    let index = 0;
-    for (let i = 0; i < count.length; i++) {
-        while (count[i]-- > 0) {
-            nums[index++] = i + min;
-        }
-    }
-
-}
-
-let nums = [8, 3, 1, 4, 52, 7, 4];
-demo(nums)
-console.log(nums)
+console.log( -1 * -4);
